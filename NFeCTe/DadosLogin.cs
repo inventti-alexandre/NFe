85f950f0cb8f6d71ce7e0a412808960debde9c3a
novatiete <viniciuscordeiro_172@hotmail.com>
2016-04-18 11:30:32 -0300

    Commit_2

---
+++ NFeCTe/DadosLogin.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NFeCTe;
+
+namespace Contabil
+{
+    public static class DadosLogin
+    {
+        public static USUARIO usuario { get; set; }
+        public static EMPRESA empresa { get; set; }
+        public static string competencia { get; set; }
+    }
+}
---
+++ NFeCTe/Main.aspx.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Contabil;
+using System.IO;
+
+namespace NFeCTe
+{
+    public partial class Main : System.Web.UI.Page
+    {
+        
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            DadosLogin.empresa = new EMPRESA();
+            DadosLogin.empresa.CNPJ_CPF = "123456789";
+        }
+
+        private void SaveFile(HttpPostedFile httpPostedFile)
+        {
+            string path = Server.MapPath(@"~\uploads\" + DadosLogin.empresa.CNPJ_CPF.ToString() + "\\");
+            DirectoryInfo dir = new DirectoryInfo(path);
+            if(!dir.Exists)
+            {
+                Directory.CreateDirectory(path);
+            }
+            foreach (var item in FileUpload1.PostedFiles)
+            {
+                if (item.ContentType == "text/xml")
+                    item.SaveAs(path + item.FileName);
+                else
+                { }
+            }
+        }
+
+        protected void btnEnviar_Click(object sender, EventArgs e)
+        {
+            if (FileUpload1.HasFile)
+                SaveFile(FileUpload1.PostedFile);
+            else
+            { }
+        }            
+    }      
+}
